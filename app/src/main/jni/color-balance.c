9bc1663b894d7955692dbf64cdf5bb9ce013bd42
Gregor Majcen <majcn.m@gmail.com>
2014-05-20 08:54:07 +0200

    update color-balance to 2.8

--- app/src/main/jni/color-balance.c
+++ app/src/main/jni/color-balance.c
@@ -1,9 +1,9 @@
 /* GIMP - The GNU Image Manipulation Program
  * Copyright (C) 1995 Spencer Kimball and Peter Mattis
  *
- * This program is free software; you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -12,8 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "color-balance.h"
@@ -25,16 +24,7 @@
 
 static void color_balance_transfer_init (void);
 
-
-/*  private variables  */
-
-static int transfer_initialized = 0;
-
-/*  for lightening  */
-static double shadows_add[256] = { 0 };
-
-/*  for darkening  */
-static double shadows_sub[256] = { 0 };
+static double shadows[256] = { 0 };
 
 
 /*  public functions  */
@@ -45,51 +35,41 @@ color_balance_init (ColorBalance *cb,
                     double        m_g,
                     double        y_b)
 {
-  cb->cyan_red = c_r;
-  cb->magenta_green = m_g;
-  cb->yellow_blue = y_b;
+    cb->cyan_red = c_r;
+    cb->magenta_green = m_g;
+    cb->yellow_blue = y_b;
 }
 
 void
 color_balance_setup (ColorBalance *cb)
 {
-  double *cyan_red_transfer;
-  double *magenta_green_transfer;
-  double *yellow_blue_transfer;
-  int i;
-  signed int r_n, g_n, b_n;
+    int i;
+    signed int r_n, g_n, b_n;
 
-  if (transfer_initialized == 0)
+    if (transfer_initialized == 0)
     {
-      color_balance_transfer_init ();
-      transfer_initialized = 1;
+        color_balance_transfer_init ();
+        transfer_initialized = 1;
     }
 
-  /*  Prepare the transfer arrays  (for speed)  */
-
-  cyan_red_transfer = (cb->cyan_red > 0) ? shadows_add : shadows_sub;
-  magenta_green_transfer = (cb->magenta_green > 0) ? shadows_add : shadows_sub;
-  yellow_blue_transfer = (cb->yellow_blue > 0) ? shadows_add : shadows_sub;
-
-  i = 256;
-  while(i--)
+    for (i = 0; i < 256; i++)
     {
-      r_n = i;
-      g_n = i;
-      b_n = i;
+        r_n = i;
+        g_n = i;
+        b_n = i;
 
-      r_n += (cb->cyan_red * cyan_red_transfer[r_n]);
-      r_n = CLAMP0255 (r_n);
+        r_n += (cb->cyan_red * shadows[i];
+        r_n = CLAMP0255 (r_n);
 
-      g_n += (cb->magenta_green * magenta_green_transfer[g_n]);
-      g_n = CLAMP0255 (g_n);
+        g_n += (cb->magenta_green * shadows[i];
+        g_n = CLAMP0255 (g_n);
 
-      b_n += (cb->yellow_blue * yellow_blue_transfer[b_n]);
-      b_n = CLAMP0255 (b_n);
+        b_n += (cb->yellow_blue * shadows[i];
+        b_n = CLAMP0255 (b_n);
 
-      cb->r_lookup[i] = r_n;
-      cb->g_lookup[i] = g_n;
-      cb->b_lookup[i] = b_n;
+        cb->r_lookup[i] = r_n;
+        cb->g_lookup[i] = g_n;
+        cb->b_lookup[i] = b_n;
     }
 }
 
@@ -98,15 +78,17 @@ color_balance_process (ColorBalance      *cb,
                        AndroidBitmapInfo *info,
                        void              *pixels)
 {
-	int xx, yy, r, g, b, r_n, g_n, b_n;
-	uint32_t *line;
+    int xx, yy, r, g, b, r_n, g_n, b_n;
+    uint32_t *line;
 
-	for(yy = 0; yy < info->height; yy++) {
-		line = (uint32_t*)pixels;
-		for(xx =0; xx < info->width; xx++) {
+    for (yy = 0; yy < info->height; yy++)
+    {
+        line = (uint32_t *)pixels;
+        for (xx = 0; xx < info->width; xx++)
+        {
             r = (int) ((line[xx] & 0x00FF0000) >> 16);
-			g = (int) ((line[xx] & 0x0000FF00) >> 8);
-			b = (int) (line[xx] & 0x00000FF );
+            g = (int) ((line[xx] & 0x0000FF00) >> 8);
+            b = (int) (line[xx] & 0x00000FF );
 
             r_n = cb->r_lookup[r];
             g_n = cb->g_lookup[g];
@@ -117,9 +99,9 @@ color_balance_process (ColorBalance      *cb,
             gimp_hsl_to_rgb_int (&r_n, &g_n, &b_n);
 
             line[xx] = ((r_n << 16) & 0x00FF0000) | ((g_n << 8) & 0x0000FF00) | (b_n & 0x000000FF);
-		}
-		pixels = (char*)pixels + info->stride;
-	}
+        }
+        pixels = (char *)pixels + info->stride;
+    }
 }
 
 
@@ -128,10 +110,13 @@ color_balance_process (ColorBalance      *cb,
 static void
 color_balance_transfer_init (void)
 {
-  int i=256;
-  while(i--)
+    int i;
+    static const double a = 64, b = 85, scale = 1.785;
+
+    for (i = 0; i < 256; i++)
     {
-      shadows_add[i] = 0.667 * (1 - SQR (((double) i - 127.0) / 127.0)); //(1.075 - 1 / ((double) i / 16.0 + 1));
-      shadows_sub[255 - i] = (1.075 - 1 / ((double) i / 16.0 + 1));
+        double low = CLAMP ((i - b) / -a + .5, 0, 1) * scale;
+
+        shadows[i] = low;
     }
 }
--- app/src/main/jni/color-balance.h
+++ app/src/main/jni/color-balance.h
@@ -1,9 +1,9 @@
 /* GIMP - The GNU Image Manipulation Program
  * Copyright (C) 1995 Spencer Kimball and Peter Mattis
  *
- * This program is free software; you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -12,8 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef __COLOR_BALANCE_H__
@@ -22,23 +21,22 @@
 #include <android/bitmap.h>
 
 
-
 typedef struct
 {
-  double cyan_red;
-  double magenta_green;
-  double yellow_blue;
+    double cyan_red;
+    double magenta_green;
+    double yellow_blue;
 
-  unsigned char r_lookup[256];
-  unsigned char g_lookup[256];
-  unsigned char b_lookup[256];
+    unsigned char r_lookup[256];
+    unsigned char g_lookup[256];
+    unsigned char b_lookup[256];
 } ColorBalance;
 
 
 void   color_balance_init    (ColorBalance      *cb,
-	                          double             c_r,
-	                          double             m_g,
-	                          double             y_b);
+                              double             c_r,
+                              double             m_g,
+                              double             y_b);
 void   color_balance_setup   (ColorBalance      *cb);
 void   color_balance_process (ColorBalance      *cb,
                               AndroidBitmapInfo *info,
