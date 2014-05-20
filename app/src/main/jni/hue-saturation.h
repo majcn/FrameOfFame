307e0415e44221ddcf19e3c8cd11a6bb41276ee4
Gregor Majcen <majcn.m@gmail.com>
2014-05-20 08:58:22 +0200

    update hue-saturation to 2.8

--- app/src/main/jni/hue-saturation.h
+++ app/src/main/jni/hue-saturation.h
@@ -1,9 +1,9 @@
 /* GIMP - The GNU Image Manipulation Program
  * Copyright (C) 1995 Spencer Kimball and Peter Mattis
  *
- * This program is free software; you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -12,8 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef __HUE_SATURATION_H__
@@ -25,24 +24,24 @@
 
 typedef struct
 {
-  double hue;
-  double lightness;
-  double saturation;
+    double hue;
+    double lightness;
+    double saturation;
 
-  int hue_transfer[256];
-  int lightness_transfer[256];
-  int saturation_transfer[256];
+    int hue_transfer[256];
+    int lightness_transfer[256];
+    int saturation_transfer[256];
 } HueSaturation;
 
 
 void   hue_saturation_init    (HueSaturation     *cb,
-	                           double             hue,
-	                           double             lightness,
-	                           double             saturation);
+                               double             hue,
+                               double             lightness,
+                               double             saturation);
 void   hue_saturation_setup   (HueSaturation     *hs);
 void   hue_saturation_process (HueSaturation     *hs,
-                              AndroidBitmapInfo *info,
-                              void              *pixels);
+                               AndroidBitmapInfo *info,
+                               void              *pixels);
 
 
 #endif  /*  __HUE_SATURATION_H__  */
