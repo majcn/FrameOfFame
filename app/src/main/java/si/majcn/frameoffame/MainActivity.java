d93c30f1724520eacf7957503f284a81fe8cbcdf
Gregor Majcen <majcn.m@gmail.com>
2014-11-11 20:25:10 +0100

    simple opencv app

--- app/src/main/java/si/majcn/frameoffame/MainActivity.java
+++ app/src/main/java/si/majcn/frameoffame/MainActivity.java
@@ -2,14 +2,80 @@ package si.majcn.frameoffame;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+import org.opencv.objdetect.CascadeClassifier;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    private static final String TAG = "FrameOfFame::Activity";
+
+    private CascadeClassifier mJavaDetector;
+    private CameraBridgeViewBase mOpenCvCameraView;
+
+    private Mat mRgba;
+    private Mat mGray;
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            if (status == LoaderCallbackInterface.SUCCESS) {
+                Log.i(TAG, "OpenCV loaded successfully");
+                mJavaDetector = OpenCVFactory.getClassifier(MainActivity.this, R.raw.lbpcascade_frontalface);
+                mOpenCvCameraView.enableView();
+            } else {
+                super.onManagerConnected(status);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.fd_activity_surface_view);
+        mOpenCvCameraView.setCvCameraViewListener(this);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_9, this, mLoaderCallback);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mGray = new Mat();
+        mRgba = new Mat();
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mGray.release();
+        mRgba.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        return inputFrame.rgba();
+    }
 }
---
+++ app/src/main/java/si/majcn/frameoffame/OpenCVFactory.java
@@ -0,0 +1,48 @@
+package si.majcn.frameoffame;
+
+import android.content.Context;
+import android.util.Log;
+
+import org.opencv.objdetect.CascadeClassifier;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class OpenCVFactory {
+
+    private static final String TAG = "FrameOfFame::OpenCVFactory";
+
+    public static CascadeClassifier getClassifier(Context context, int classifierResourceId) {
+        CascadeClassifier javaDetector = null;
+
+        File cascadeDir = context.getDir("cascade", Context.MODE_PRIVATE);
+        try {
+            // load cascade file from application resources
+            InputStream is = context.getResources().openRawResource(classifierResourceId);
+            File cascadeFile = new File(cascadeDir, "classifier.xml");
+            FileOutputStream os = new FileOutputStream(cascadeFile);
+
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = is.read(buffer)) != -1) {
+                os.write(buffer, 0, bytesRead);
+            }
+            is.close();
+            os.close();
+
+            javaDetector = new CascadeClassifier(cascadeFile.getAbsolutePath());
+            if (javaDetector.empty()) {
+                Log.e(TAG, "Failed to load cascade classifier");
+                javaDetector = null;
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to load cascade. Exception thrown: " + e);
+        } finally {
+            cascadeDir.delete();
+        }
+
+        return javaDetector;
+    }
+}
