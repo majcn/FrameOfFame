a7f0083f9bfa79f9d0f6ca2d516e8a152fe8cf66
Gregor Majcen <majcn.m@gmail.com>
2014-11-20 23:50:08 +0100

    malo poglej: TODO: CustomContext prek serializable

--- app/src/main/java/si/majcn/frameoffame/fragment/CameraFragment.java
+++ app/src/main/java/si/majcn/frameoffame/fragment/CameraFragment.java
@@ -1,62 +1,52 @@
 package si.majcn.frameoffame.fragment;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
-import org.opencv.core.MatOfRect;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
-import org.opencv.android.CameraBridgeViewBase;
-import org.opencv.objdetect.CascadeClassifier;
-import org.opencv.objdetect.Objdetect;
 
 import si.majcn.frameoffame.CustomContext;
 import si.majcn.frameoffame.MainActivity;
-import si.majcn.frameoffame.opencv.OpenCVFactory;
 import si.majcn.frameoffame.R;
+import si.majcn.frameoffame.opencv.FaceDetector;
 
 /**
  * Created by majcn on 19.11.2014.
  */
 public class CameraFragment extends Fragment implements CameraBridgeViewBase.CvCameraViewListener2 {
 
-    private MainActivity context;
-
-    private static final String TAG = "FrameOfFame::CameraFragment";
-
-    private static final int FACE_DETECT_FLAGS = Objdetect.CASCADE_FIND_BIGGEST_OBJECT | Objdetect.CASCADE_DO_ROUGH_SEARCH;
+    private Context mContext;
+    private CustomContext mCustomContext;
     private static final Scalar FACE_RECT_COLOR = new Scalar(0, 255, 0, 255);
 
 
-    private CascadeClassifier mJavaDetector;
+    private FaceDetector mJavaDetector;
     private CameraBridgeViewBase mOpenCvCameraView;
 
     private Mat mRgba;
     private Mat mGray;
     private Mat mRealView;
 
-    private int mAbsoluteFaceSize = 0;
-    private float mRelativeFaceSize = 0.2f;
-
-    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(context) {
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(mContext) {
         @Override
         public void onManagerConnected(int status) {
             if (status == LoaderCallbackInterface.SUCCESS) {
-                Log.i(TAG, "OpenCV loaded successfully");
-
-                mJavaDetector = OpenCVFactory.getClassifier(context, R.raw.haarcascade_frontalface_default);
+                mJavaDetector = new FaceDetector(mContext);
+                mJavaDetector.start();
                 mOpenCvCameraView.enableView();
             } else {
                 super.onManagerConnected(status);
@@ -68,14 +58,16 @@ public class CameraFragment extends Fragment implements CameraBridgeViewBase.CvC
     public void onAttach(Activity activity) {
         super.onAttach(activity);
 
-        context = (MainActivity)activity;
+        mContext = activity;
+        mCustomContext = ((MainActivity)activity).getCustomContext();
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
 
-        context = null;
+        mContext = null;
+	mCustomContext = null;
     }
 
     @Override
@@ -92,18 +84,20 @@ public class CameraFragment extends Fragment implements CameraBridgeViewBase.CvC
     @Override
     public void onResume() {
         super.onResume();
-        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_9, context, mLoaderCallback);
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_9, mContext, mLoaderCallback);
     }
 
     @Override
     public void onPause() {
         super.onPause();
+        mJavaDetector.stop();
         mOpenCvCameraView.disableView();
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        mJavaDetector.stop();
         mOpenCvCameraView.disableView();
     }
 
@@ -126,40 +120,17 @@ public class CameraFragment extends Fragment implements CameraBridgeViewBase.CvC
         mRgba = inputFrame.rgba();
         mGray = inputFrame.gray();
 
-        if (mAbsoluteFaceSize == 0) {
-            int height = mGray.height();
-            if (Math.round(height * mRelativeFaceSize) > 0) {
-                mAbsoluteFaceSize = Math.round(height * mRelativeFaceSize);
-            }
-        }
-
-        MatOfRect faces = new MatOfRect();
-        mJavaDetector.detectMultiScale(mGray, faces, 1.05, 3, FACE_DETECT_FLAGS, new Size(mAbsoluteFaceSize, mAbsoluteFaceSize), new Size());
-        Rect[] facesArray = faces.toArray();
-        Rect curFace = null;
-        if (facesArray.length > 0) {
-            Rect face = facesArray[0];
-            curFace = getResizedRect(face, mRgba.width(), mRgba.height());
-
-            CustomContext cc = context.getCustomContext();
+        Rect curFace = mJavaDetector.detectOne(mGray);
+        if (curFace != null) {
             Mat intermediateMat = mRgba.submat(curFace);
-            Imgproc.resize(intermediateMat, mRealView, new Size(cc.getImageSize(), cc.getImageSize()));
+            Imgproc.resize(intermediateMat, mRealView, new Size(mCustomContext.getImageSize(), mCustomContext.getImageSize()));
             intermediateMat.release();
-            cc.fillBitmap(mRealView);
+            mCustomContext.fillBitmap(mRealView);
         }
 
         if (curFace != null) {
             Core.rectangle(mGray, curFace.tl(), curFace.br(), FACE_RECT_COLOR, 3);
         }
         return mGray;
-    }
-
-    private Rect getResizedRect(Rect orig, int maxWidth, int maxHeight) {
-        int x = Math.max(orig.x - orig.width / 4, 0);
-        int w = Math.min(orig.width + orig.width / 2, maxWidth - x);
-        int y = Math.max(orig.y - orig.height / 4, 0);
-        int h = Math.min(orig.height + orig.height / 2, maxHeight - y);
-
-        return new Rect(x, y, w, h);
     }
 }
---
+++ app/src/main/java/si/majcn/frameoffame/opencv/FaceDetector.java
@@ -0,0 +1,83 @@
+package si.majcn.frameoffame.opencv;
+
+import android.content.Context;
+
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Rect;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+import org.opencv.objdetect.Objdetect;
+
+import si.majcn.frameoffame.CustomContext;
+import si.majcn.frameoffame.R;
+
+/**
+ * Created by majcn on 20.11.2014.
+ */
+public class FaceDetector {
+
+    private static final int FACE_DETECT_FLAGS_ONE = Objdetect.CASCADE_FIND_BIGGEST_OBJECT | Objdetect.CASCADE_DO_ROUGH_SEARCH;
+    private static final double SCALE_FACTOR = 1.05;
+    private static final int MIN_NEIGHBORS = 3;
+
+    private int mAbsoluteFaceSizeMin = 0;
+    private float mRelativeFaceSizeMin = 0.2f;
+
+    private int mAbsoluteFaceSizeMax = 0;
+    private float mRelativeFaceSizeMax = 0.5f;
+
+    private CascadeClassifier mFaceDetector;
+//    private CascadeClassifier eyeDetector;
+
+    public FaceDetector(Context context) {
+        mFaceDetector = OpenCVFactory.getClassifier(context, R.raw.haarcascade_frontalface_default);
+    }
+
+    public void start() {
+
+    }
+
+    public void stop() {
+
+    }
+
+    public Rect detectOne(Mat image) {
+        if (mAbsoluteFaceSizeMin == 0) {
+            int height = image.height();
+            if (Math.round(height * mRelativeFaceSizeMin) > 0) {
+                mAbsoluteFaceSizeMin = Math.round(height * mRelativeFaceSizeMin);
+            }
+        }
+
+
+        if (mAbsoluteFaceSizeMax == 0) {
+            int height = image.height();
+            if (Math.round(height * mRelativeFaceSizeMax) > 0) {
+                mAbsoluteFaceSizeMax = Math.round(height * mRelativeFaceSizeMax);
+            }
+        }
+
+        MatOfRect faces = new MatOfRect();
+        mFaceDetector.detectMultiScale(image, faces, SCALE_FACTOR, MIN_NEIGHBORS, FACE_DETECT_FLAGS_ONE, new Size(mAbsoluteFaceSizeMin, mAbsoluteFaceSizeMin), new Size(mAbsoluteFaceSizeMax, mAbsoluteFaceSizeMax));
+
+        Rect[] facesArray = faces.toArray();
+        Rect curFace = null;
+        if (facesArray.length > 0) {
+            curFace = getResizedRect(facesArray[0], image.width(), image.height());
+        }
+        faces.release();
+
+        return curFace;
+    }
+
+    private Rect getResizedRect(Rect orig, int maxWidth, int maxHeight) {
+        int x = Math.max(orig.x - orig.width / 4, 0);
+        int w = Math.min(orig.width + orig.width / 2, maxWidth - x);
+        int y = Math.max(orig.y - orig.height / 4, 0);
+        int h = Math.min(orig.height + orig.height / 2, maxHeight - y);
+
+        return new Rect(x, y, w, h);
+    }
+}
